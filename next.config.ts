b8b74ee265e36e6f1b90e430be82908d0c7925bc
Villeprat Romain <romain@Mac.lan>
2026-05-03 22:06:53 +0200

    Fix redirect whatmarket.fr vers /whatmarket

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,22 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [{ type: 'host', value: 'whatmarket.fr' }],
+        destination: '/whatmarket',
+        permanent: false,
+      },
+      {
+        source: '/',
+        has: [{ type: 'host', value: 'www.whatmarket.fr' }],
+        destination: '/whatmarket',
+        permanent: false,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
